3bc97025b81ff39374c2085ea162f2a87134f4b0
Charles Perkins <chuck@kuracali.com>
2019-04-27 20:38:37 -0700

    why does hexdump allow this to work when commenting it out causes it to hang?

--- ok.asm
+++ ok.asm
@@ -6,7 +6,7 @@ section '.text' code executable readable
 include 'efi.inc'
 
 main:
- sub rsp, 4*8              ; reserve space for 4 arguments
+ sub rsp, 6*8              ; reserve space for 6 arguments
 
  mov [Handle], rcx         ; ImageHandle
  mov [SystemTable], rdx    ; pointer to SystemTable
@@ -16,63 +16,87 @@ main:
  mov rcx, [rcx + EFI_SYSTEM_TABLE.ConOut]
  call [rcx + SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString]
 
- ; get the memory map
-
- mov dword [memmapdescsize], 48
+ mov qword [memmapsize], 4096
  lea rcx, [memmapsize]
- mov qword [rcx], 4096
- lea rdx, [memmapbuff]
+ lea rdx, [memmap]
  lea r8, [memmapkey]
  lea r9, [memmapdescsize]
- lea rax, [memmapdescver]
- push rax
- mov rax, [SystemTable]
- mov rax, [rax + EFI_SYSTEM_TABLE.BootServices]
- call [rax + EFI_BOOT_SERVICES.GetMemoryMap]
- pop rcx
- test rax, rax
- jnz oops
- 
+ lea r10, [memmapdescver]
+ mov [STK],rsp
+ push r10
+ sub rsp, 4*8
+ mov rbx, [SystemTable]
+ mov rbx, [rbx + EFI_SYSTEM_TABLE.BootServices]
+ call [rbx + EFI_BOOT_SERVICES.GetMemoryMap]
+ add rsp, 4*8
+ pop r10
+ mov rsp, [STK]
+ cmp rax, EFI_SUCCESS
+ jne oops
+
+       mov rbx, [memmapkey]
+       push rax
+       push rcx
+       push rdx
+       call printhex
+       pop rdx
+       pop rcx
+       pop rax
+
+
  mov rcx, [Handle]
  mov rdx, [memmapkey]
- mov rax, [SystemTable]
- mov rax, [rax + EFI_SYSTEM_TABLE.BootServices]
- call [rax + EFI_BOOT_SERVICES.ExitBootServices]
+ mov rbx, [SystemTable]
+ mov rbx, [rbx + EFI_SYSTEM_TABLE.BootServices]
+ call [rbx + EFI_BOOT_SERVICES.ExitBootServices]
  cmp rax, EFI_SUCCESS
  je reboot
 
  ; if first time fails, second time is supposed to succeed:
 
- mov dword [memmapdescsize], 48
+ mov qword [memmapsize], 4096
  lea rcx, [memmapsize]
- mov qword [rcx], 4096
- lea rdx, [memmapbuff]
+ lea rdx, [memmap]
  lea r8, [memmapkey]
  lea r9, [memmapdescsize]
- lea rax, [memmapdescver]
- push rax
- mov rax, [SystemTable]
- mov rax, [rax + EFI_SYSTEM_TABLE.BootServices]
- call [rax + EFI_BOOT_SERVICES.GetMemoryMap]
- pop rcx
- test rax, rax
- jnz oops
+ lea r10, [memmapdescver]
+ mov [STK],rsp
+ push r10
+ sub rsp, 4*8
+ mov rax, 5
+ mov rbx, [SystemTable]
+ mov rbx, [rbx + EFI_SYSTEM_TABLE.BootServices]
+ call [rbx + EFI_BOOT_SERVICES.GetMemoryMap]
+ add rsp, 4*8
+ pop r10
+ mov rsp, [STK]
+ cmp rax, EFI_SUCCESS
+ jne oops
+
+       mov rbx, [memmapkey]
+       push rax
+       push rcx
+       push rdx
+       call printhex
+       pop rdx
+       pop rcx
+       pop rax
 
  mov rcx, [Handle]
  mov rdx, [memmapkey]
- mov rax, [SystemTable]
- mov rax, [rax + EFI_SYSTEM_TABLE.BootServices]
- call [rax + EFI_BOOT_SERVICES.ExitBootServices]
-    
-
+ mov rbx, [SystemTable]
+ mov rbx, [rbx + EFI_SYSTEM_TABLE.BootServices]
+ call [rbx + EFI_BOOT_SERVICES.ExitBootServices]
+ 
 reboot:
- mov rax, [SystemTable]
- mov rax, [rax + EFI_SYSTEM_TABLE.RuntimeServices]
+
+ mov rbx, [SystemTable]
+ mov rbx, [rbx + EFI_SYSTEM_TABLE.RuntimeServices]
+
  mov rcx, EfiResetShutdown
  mov rdx, EFI_SUCCESS
  xor r8, r8
- xor r9, r9
- call [rax + EFI_RUNTIME_SERVICES.ResetSystem]
+ call [rbx + EFI_RUNTIME_SERVICES.ResetSystem]
  jmp oops
 
     
@@ -81,29 +105,75 @@ oops:
  mov rcx, [SystemTable]
  mov rcx, [rcx + EFI_SYSTEM_TABLE.ConOut]
  call [rcx + SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString]
+A:
+ jmp A
 
- jmp $-1
+eek:
+ lea rdx, [_eek]
+ mov rcx, [SystemTable]
+ mov rcx, [rcx + EFI_SYSTEM_TABLE.ConOut]
+ call [rcx + SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString]
+B:
+ jmp B
 
- add rsp, 4*8
+
+boop:
+ lea rdx, [_boop]
+ mov rcx, [SystemTable]
+ mov rcx, [rcx + EFI_SYSTEM_TABLE.ConOut]
+ call [rcx + SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString]
+ret
+
+
+ add rsp, 6*8
  mov eax, 0 ; was EFI_SUCCESS
  retn
 
+printhex:
+ mov rbp, 16
+.loop:
+	rol rbx, 4
+	mov rax, rbx
+	and rax, 0Fh
+	lea rcx, [_Hex]
+	mov rax, [rax + rcx]
+	mov byte [_Num], al
+        lea rdx, [_Num]
+        mov rcx, [SystemTable]
+        mov rcx, [rcx + EFI_SYSTEM_TABLE.ConOut]
+        call [rcx + SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString]
+	dec rbp
+ jnz .loop
+ lea rdx, [_Nl]
+ mov rcx, [SystemTable]
+ mov rcx, [rcx + EFI_SYSTEM_TABLE.ConOut] 
+ call [rcx + SIMPLE_TEXT_OUTPUT_INTERFACE.OutputString]
+
+ ret
+
 
 section '.data' data readable writeable
-
-memmapbuff: rb 4096
-
+memmap:	    times 4096 db 0
 Handle      dq 0
 SystemTable dq 0
-
-memmapsize:     dq 4096
+;RTS	    dq 0
+;BS	    dq 0
+STK	    dq 0
+ptrmemmap:	dq 0
+memmapsize:     dq 0
 memmapkey:      dq 0
 memmapdescsize: dq 0
 memmapdescver:  dq 0
 
 _yok      du 'ok.',13,10,0
 _nok	  du 'not ok.',13,10,0
+_eek      du 'eek!',13,10,0
+_boop     du 'boop',13,10,0
 _hello du 'hello world',13,10,0
+_Hex					db '0123456789ABCDEF'
+_Num					dw 0,0
+_Nl					dw 13,10,0
+
 
 
 section '.reloc' fixups data discardable
